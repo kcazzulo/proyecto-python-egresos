8fa81b9c69730812dd9ebdb3d66b2a54802ce0e7
Karen Cazzulo <karen.cazzulo@outlook.com>
2026-06-04 21:15:46 -0300

    "actualizo app"

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import os
+
+# --- CONFIGURACIÓN ---
+st.set_page_config(page_title="Análisis de Egresos Hospitalarios", layout="wide")
+
+
 # --- VISUALIZACIÓN DINÁMICA ---
 st.subheader("📈 VISUALIZACIÓN DINÁMICA")
 
